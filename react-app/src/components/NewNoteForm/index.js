5f3349bc8750192b37aed95095d6d2f8502385c1
Barry Mattern <bdmattern@gmail.com>
2022-07-25 16:32:56 -0600

    Add simple frontend error handling to `NewNoteForm`

--- react-app/src/components/NewNoteForm/index.js
+++ react-app/src/components/NewNoteForm/index.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchAllNotes, fetchNewNote } from "../../store/notesReducer";
 
 const NewNoteForm = () => {
+    const [errors, setErrors] = useState([]);
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const dispatch = useDispatch();
@@ -18,35 +19,55 @@ const NewNoteForm = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const note = await dispatch(fetchNewNote(user.id, title, body));
-        history.push(`/notes/${note.id}`);
+
+        const errors = [];
+        
+        if (!title) errors.push('Please add a title.');
+        if (!body) errors.push('Please add a body.');
+        setErrors(errors);
+
+        if (errors.length === 0) {
+            const note = await dispatch(fetchNewNote(user.id, title, body));
+            if (note) history.push(`/notes/${note.id}`);
+        }
     };
 
     const updateTitle = (e) => setTitle(e.target.value);
     const updateBody = (e) => setBody(e.target.value);
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor='title' >Title </label>
-                <input
-                    type='text'
-                    className='title'
-                    placeholder='Title of Note'
-                    value={title}
-                    onChange={updateTitle}
-                />
-                <label htmlFor='title' >Body </label>
-                <textarea
-                    type='text'
-                    className='body'
-                    placeholder='What do you want to write?'
-                    value={body}
-                    onChange={updateBody}
-                />
-                <input type='submit' value='Submit' />
-            </form>
-        </div>
+        <>
+            {errors.length > 0 &&
+                <div className='form-errors'>
+                    <ul>
+                        {errors.map((error, i) => (
+                            <li key={i}>{error}</li>
+                        ))}
+                    </ul>
+                </div>
+            }
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor='title' >Title </label>
+                    <input
+                        type='text'
+                        className='title'
+                        placeholder='Title of Note'
+                        value={title}
+                        onChange={updateTitle}
+                    />
+                    <label htmlFor='title' >Body </label>
+                    <textarea
+                        type='text'
+                        className='body'
+                        placeholder='What do you want to write?'
+                        value={body}
+                        onChange={updateBody}
+                    />
+                    <input type='submit' value='Submit' />
+                </form>
+            </div>
+        </>
     );
 };
 
